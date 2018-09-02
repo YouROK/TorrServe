154ce9d67590b338bea1197c4a5731b9ed160a73
yourok <8yourok8@mail.ru>
2018-09-02 22:17:20 +0300

    fix no play on unknown pkg

--- app/src/main/java/ru/yourok/torrserve/activitys/play/PlayActivity.kt
+++ app/src/main/java/ru/yourok/torrserve/activitys/play/PlayActivity.kt
@@ -202,6 +202,7 @@ class PlayActivity : AppCompatActivity() {
                     finish()
                     return
                 }
+                intent.`package` = ""
             }
 
             val intentC = Intent.createChooser(intent, "")
