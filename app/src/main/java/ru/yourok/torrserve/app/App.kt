a27547a28f4e2e3a4539d621a17a4cfba935b9e1
yourok <8yourok8@mail.ru>
2018-09-18 18:22:15 +0300

    add wakelock

--- app/src/main/java/ru/yourok/torrserve/app/App.kt
+++ app/src/main/java/ru/yourok/torrserve/app/App.kt
@@ -4,15 +4,21 @@ import android.app.Application
 import android.content.Context
 import android.os.Handler
 import android.os.Looper
+import android.os.PowerManager
 import ru.yourok.torrserve.R
 
 
 class App : Application() {
     companion object {
         private lateinit var contextApp: Context
+        private lateinit var wakeLock: PowerManager.WakeLock
 
         fun getContext(): Context {
             return contextApp
+        }
+
+        fun wakeLock(timeout: Long) {
+            wakeLock.acquire(timeout)
         }
 
         fun Toast(txt: String) {
@@ -25,6 +31,10 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         contextApp = applicationContext
+
+        val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
+        wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "TorrServe:WakeLock")
+
         ACR.get(this)
                 .setEmailAddresses("8yourok8@gmail.com")
                 .setEmailSubject(getString(R.string.app_name) + " Crash Report")
