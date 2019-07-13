e5b3a1ba0cc3330ae937fb8301f2b591ad1e299b
yourok <8yourok8@mail.ru>
2019-07-13 15:19:49 +0300

    fix auto start in android 8+

--- app/src/main/java/ru/yourok/torrserve/services/BootStart.kt
+++ app/src/main/java/ru/yourok/torrserve/services/BootStart.kt
@@ -3,30 +3,30 @@ package ru.yourok.torrserve.services
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.Build
-import android.os.Handler
-import ru.yourok.torrserve.R
-import ru.yourok.torrserve.app.App
+import ru.yourok.torrserve.activitys.splash.SplashActivity
 import ru.yourok.torrserve.preferences.Preferences
 
 class BootCompletedReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (Preferences.isAutoStart()) {
-            val intent = Intent(context, ServerService::class.java)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-                Handler().post {
-                    try {
-                        context.startForegroundService(intent)
-                    } catch (e: Exception) {
-                        App.Toast(R.string.error_server_start)
-                    }
-                }
-            else
-                try {
-                    context.startService(intent)
-                } catch (e: Exception) {
-                    App.Toast(R.string.error_server_start)
-                }
+            val intent = Intent(context, SplashActivity::class.java)
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(intent)
+//            val intent = Intent(context, ServerService::class.java)
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+//                Handler().post {
+//                    try {
+//                        context.startForegroundService(intent)
+//                    } catch (e: Exception) {
+//                        App.Toast(R.string.error_server_start)
+//                    }
+//                }
+//            else
+//                try {
+//                    context.startService(intent)
+//                } catch (e: Exception) {
+//                    App.Toast(R.string.error_server_start)
+//                }
         }
     }
 }
