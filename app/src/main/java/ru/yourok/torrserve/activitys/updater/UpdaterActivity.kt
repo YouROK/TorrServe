080425354b7b1d30687056d3049e8c8c42fdd8a9
yourok <8yourok8@mail.ru>
2019-08-16 18:50:08 +0300

    refactor and add custom servers

--- app/src/main/java/ru/yourok/torrserve/activitys/updater/UpdaterActivity.kt
+++ app/src/main/java/ru/yourok/torrserve/activitys/updater/UpdaterActivity.kt
@@ -1,8 +1,10 @@
 package ru.yourok.torrserve.activitys.updater
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import android.os.*
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.*
@@ -11,9 +13,11 @@ import ru.yourok.torrserve.BuildConfig
 import ru.yourok.torrserve.R
 import ru.yourok.torrserve.dialog.DialogList
 import ru.yourok.torrserve.server.api.Api
+import ru.yourok.torrserve.server.updater.Releases
 import ru.yourok.torrserve.serverloader.ServerFile
 import ru.yourok.torrserve.serverloader.Updater
 import kotlin.concurrent.thread
+
 
 class UpdaterActivity : AppCompatActivity() {
 
@@ -43,15 +47,21 @@ class UpdaterActivity : AppCompatActivity() {
             }
         }
 
-        findViewById<Button>(R.id.update_remotly_button).setOnClickListener {
+        findViewById<Button>(R.id.btnInstallRemotly).setOnClickListener {
             thread {
-                updateRemotly()
+                installRemotly()
             }
         }
 
-        findViewById<Button>(R.id.update_local_button).setOnClickListener {
+        findViewById<Button>(R.id.btnInstallCustom).setOnClickListener {
             thread {
-                updateLocaly()
+                installCustom()
+            }
+        }
+
+        findViewById<Button>(R.id.btnInstallLocal).setOnClickListener {
+            thread {
+                installLocaly()
             }
         }
 
@@ -131,12 +141,9 @@ class UpdaterActivity : AppCompatActivity() {
         }
     }
 
-    fun updateRemotly() {
-        Handler(Looper.getMainLooper()).post {
-            findViewById<ProgressBar>(R.id.progress_bar).visibility = View.VISIBLE
-            findViewById<ProgressBar>(R.id.progress_bar).isIndeterminate = false
-            findViewById<TextView>(R.id.update_info).setText(R.string.downloading)
-        }
+    fun installRemotly() {
+        showProgress(true, getString(R.string.downloading))
+
         try {
             Updater.updateServerRemote(testVersion) { prc ->
                 Handler(Looper.getMainLooper()).post {
@@ -150,7 +157,7 @@ class UpdaterActivity : AppCompatActivity() {
                 findViewById<ProgressBar>(R.id.progress_bar).isIndeterminate = true
                 findViewById<TextView>(R.id.update_info).setText("")
             }
-            Thread.sleep(1000)
+            Thread.sleep(2000)
             checkVersion()
         } catch (e: Exception) {
             Handler(Looper.getMainLooper()).post {
@@ -162,7 +169,79 @@ class UpdaterActivity : AppCompatActivity() {
         }
     }
 
-    fun updateLocaly() {
+    fun installCustom() {
+        try {
+            showProgress(true, "")
+
+            val releases = Releases.get()
+            val builderSingle = AlertDialog.Builder(this@UpdaterActivity)
+
+            val arrayAdapter = ArrayAdapter<String>(this@UpdaterActivity, android.R.layout.select_dialog_singlechoice)
+            releases.forEach {
+                arrayAdapter.add(it.Version)
+            }
+
+            builderSingle.setNegativeButton(android.R.string.cancel, DialogInterface.OnClickListener { dialog, which ->
+                dialog.dismiss()
+                showProgress(false, "")
+            })
+
+            builderSingle.setAdapter(arrayAdapter, DialogInterface.OnClickListener { dialog, which ->
+                try {
+                    dialog.dismiss()
+                    thread {
+                        val rel = releases[which]
+                        val arch = Updater.getArch()
+                        val link = rel.Links["android-${arch}"]
+                        if (link == null) {
+                            showProgress(false, getString(R.string.warn_error_download_server))
+                            return@thread
+                        }
+
+                        Handler(Looper.getMainLooper()).post {
+                            findViewById<ProgressBar>(R.id.progress_bar).setProgress(0)
+                            findViewById<ProgressBar>(R.id.progress_bar).isIndeterminate = false
+                            findViewById<TextView>(R.id.update_info).setText(R.string.downloading)
+                        }
+                        Releases.updateCustomServer(link) { prc ->
+                            Handler(Looper.getMainLooper()).post {
+                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+                                    findViewById<ProgressBar>(R.id.progress_bar).setProgress(prc, true)
+                                else
+                                    findViewById<ProgressBar>(R.id.progress_bar).setProgress(prc)
+                            }
+                        }
+                        Handler(Looper.getMainLooper()).post {
+                            findViewById<ProgressBar>(R.id.progress_bar).isIndeterminate = true
+                            findViewById<TextView>(R.id.update_info).setText("")
+                        }
+                        Thread.sleep(2000)
+                        checkVersion()
+                    }
+                } catch (e: Exception) {
+                    Handler(Looper.getMainLooper()).post {
+                        val msg = "Error download server: " + (e.message ?: e.cause?.toString() ?: "")
+                        findViewById<TextView>(R.id.update_info).setText(msg)
+                        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+                        findViewById<ProgressBar>(R.id.progress_bar).visibility = View.GONE
+                    }
+                }
+            })
+
+            Handler(Looper.getMainLooper()).post {
+                builderSingle.show()
+            }
+        } catch (e: Exception) {
+            Handler(Looper.getMainLooper()).post {
+                val msg = "Error download server: " + (e.message ?: e.cause?.toString() ?: "")
+                findViewById<TextView>(R.id.update_info).setText(msg)
+                Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+                findViewById<ProgressBar>(R.id.progress_bar).visibility = View.GONE
+            }
+        }
+    }
+
+    fun installLocaly() {
         val dw = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
         val files = dw.listFiles { file ->
             if (file.isFile)
@@ -199,6 +278,18 @@ class UpdaterActivity : AppCompatActivity() {
                     }
                 }
             }
+        }
+    }
+
+    private fun showProgress(show: Boolean, msg: String) {
+        Handler(Looper.getMainLooper()).post {
+            if (show)
+                findViewById<ProgressBar>(R.id.progress_bar).visibility = View.VISIBLE
+            else
+                findViewById<ProgressBar>(R.id.progress_bar).visibility = View.GONE
+
+            findViewById<ProgressBar>(R.id.progress_bar).isIndeterminate = true
+            findViewById<TextView>(R.id.update_info).setText(msg)
         }
     }
 }
