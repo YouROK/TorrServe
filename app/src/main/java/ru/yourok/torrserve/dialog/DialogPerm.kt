1dd3a87f679f1d61599aed9cc6813025172f378a
yourok <8yourok8@mail.ru>
2019-10-18 20:50:27 +0300

    remove power manager sets

--- app/src/main/java/ru/yourok/torrserve/dialog/DialogPerm.kt
+++ app/src/main/java/ru/yourok/torrserve/dialog/DialogPerm.kt
@@ -2,13 +2,6 @@ package ru.yourok.torrserve.dialog
 
 import android.Manifest
 import android.app.Activity
-import android.content.Context
-import android.content.Intent
-import android.os.Build
-import android.os.Handler
-import android.os.Looper
-import android.os.PowerManager
-import android.provider.Settings
 import android.support.design.widget.Snackbar
 import android.support.v4.app.ActivityCompat
 import android.view.View
@@ -28,17 +21,17 @@ object DialogPerm {
                 ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
             }
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                Handler(Looper.getMainLooper()).postDelayed({
-                    val packageName = activity.getPackageName()
-                    val pm = activity.getSystemService(Context.POWER_SERVICE) as PowerManager
-                    if (!pm.isIgnoringBatteryOptimizations(packageName)) {
-                        val intent = Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS)
-                        if (intent.resolveActivity(activity.packageManager) != null)
-                            activity.startActivity(intent)
-                    }
-                }, 2000)
-            }
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                Handler(Looper.getMainLooper()).postDelayed({
+//                    val packageName = activity.getPackageName()
+//                    val pm = activity.getSystemService(Context.POWER_SERVICE) as PowerManager
+//                    if (!pm.isIgnoringBatteryOptimizations(packageName)) {
+//                        val intent = Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS)
+//                        if (intent.resolveActivity(activity.packageManager) != null)
+//                            activity.startActivity(intent)
+//                    }
+//                }, 2000)
+//            }
         }
     }
 }
