6931a5f4bbf22f676e84fbcb9be52434d3795a4c
yourok <8yourok8@mail.ru>
2020-04-12 19:59:14 +0300

    swipe menu on all window size

--- app/src/main/java/ru/yourok/torrserve/activitys/main/MainActivity.kt
+++ app/src/main/java/ru/yourok/torrserve/activitys/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.yourok.torrserve.activitys.main
 
 import android.content.Intent
+import android.graphics.Point
 import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
@@ -35,6 +36,7 @@ import ru.yourok.torrserve.server.net.Net
 import ru.yourok.torrserve.serverloader.ServerFile
 import ru.yourok.torrserve.serverloader.Updater
 import ru.yourok.torrserve.services.ServerService
+import java.lang.reflect.Field
 import kotlin.concurrent.thread
 
 
@@ -157,6 +159,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupNavigator() {
+
+        //Set swipe from the center of the screen
+        val mDrawerLayout = findViewById<DrawerLayout>(R.id.drawerlayout)
+        val mDragger: Field = mDrawerLayout.javaClass.getDeclaredField("mLeftDragger")
+        val display = windowManager.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+        val width: Int = size.x
+
+        mDragger.isAccessible = true
+        val draggerObj = mDragger.get(mDrawerLayout)
+        val mEdgeSize: Field = draggerObj.javaClass.getDeclaredField("mEdgeSize")
+        mEdgeSize.isAccessible = true
+        mEdgeSize.setInt(draggerObj, width)
+
+
         val tvCurrHost = findViewById<TextView>(R.id.tvCurrentHost)
         tvCurrHost.text = Preferences.getCurrentHost()
 
