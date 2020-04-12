54e647d7b7b6ae5e0173e5ea11d00db924579f4c
yourok <8yourok8@mail.ru>
2020-04-12 20:00:14 +0300

    catch errors

--- app/src/main/java/ru/yourok/torrserve/activitys/main/MainActivity.kt
+++ app/src/main/java/ru/yourok/torrserve/activitys/main/MainActivity.kt
@@ -161,18 +161,22 @@ class MainActivity : AppCompatActivity() {
     private fun setupNavigator() {
 
         //Set swipe from the center of the screen
-        val mDrawerLayout = findViewById<DrawerLayout>(R.id.drawerlayout)
-        val mDragger: Field = mDrawerLayout.javaClass.getDeclaredField("mLeftDragger")
-        val display = windowManager.defaultDisplay
-        val size = Point()
-        display.getSize(size)
-        val width: Int = size.x
+        try {
+            val mDrawerLayout = findViewById<DrawerLayout>(R.id.drawerlayout)
+            val mDragger: Field = mDrawerLayout.javaClass.getDeclaredField("mLeftDragger")
+            val display = windowManager.defaultDisplay
+            val size = Point()
+            display.getSize(size)
+            val width: Int = size.x
 
-        mDragger.isAccessible = true
-        val draggerObj = mDragger.get(mDrawerLayout)
-        val mEdgeSize: Field = draggerObj.javaClass.getDeclaredField("mEdgeSize")
-        mEdgeSize.isAccessible = true
-        mEdgeSize.setInt(draggerObj, width)
+            mDragger.isAccessible = true
+            val draggerObj = mDragger.get(mDrawerLayout)
+            val mEdgeSize: Field = draggerObj.javaClass.getDeclaredField("mEdgeSize")
+            mEdgeSize.isAccessible = true
+            mEdgeSize.setInt(draggerObj, width)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
 
 
         val tvCurrHost = findViewById<TextView>(R.id.tvCurrentHost)
