d98eb9aeab1b26957ee2033b13c2b7ce5eb084e2
yourok <8yourok8@mail.ru>
2020-05-26 23:31:16 +0300

    add in add activity poster and title

--- app/src/main/java/ru/yourok/torrserve/activitys/add/AddActivity.kt
+++ app/src/main/java/ru/yourok/torrserve/activitys/add/AddActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_add.*
+import org.json.JSONObject
 import ru.yourok.torrserve.R
 import ru.yourok.torrserve.app.App
 import ru.yourok.torrserve.server.api.Api
@@ -28,7 +29,18 @@ class AddActivity : AppCompatActivity() {
         buttonAdd.setOnClickListener {
             thread {
                 try {
-                    Api.torrentAdd(editTextTorrLink.text.toString().trim(), "", "", true)
+                    val title = editTextTorrTitle?.text?.toString() ?: ""
+                    val poster = editTextTorrPoster?.text?.toString() ?: ""
+                    var info = ""
+
+                    val js = JSONObject()
+                    if (poster.isNotEmpty())
+                        js.put("poster_path", poster)
+                    if (title.isNotEmpty())
+                        js.put("title", title)
+                    info = js.toString(0)
+
+                    Api.torrentAdd(editTextTorrLink.text.toString().trim(), title, info, true)
                     finish()
                 } catch (e: Exception) {
                     val msg = e.message ?: getString(R.string.error_add_torrent)
