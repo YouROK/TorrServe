2edd8aeb6b6c920910a4be9442336f47dc4f7277
YouROK <8yourok8@mail.ru>
2020-11-20 17:09:53 +0300

    update

--- app/src/main/java/ru/yourok/torrserve/ui/fragments/add/AddFragment.kt
+++ app/src/main/java/ru/yourok/torrserve/ui/fragments/add/AddFragment.kt
@@ -1,12 +1,16 @@
 package ru.yourok.torrserve.ui.fragments.add
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import ru.yourok.torrserve.R
+import ru.yourok.torrserve.ui.activities.play.PlayActivity
 import ru.yourok.torrserve.ui.fragments.fragManager
 
 class AddFragment : Fragment() {
@@ -28,7 +32,17 @@ class AddFragment : Fragment() {
 
         view?.apply {
             findViewById<Button>(R.id.btnOK)?.setOnClickListener {
-                //TODO add torrent
+                val link = view?.findViewById<EditText>(R.id.etMagnet)?.text
+                val title = view?.findViewById<EditText>(R.id.etTitle)?.text
+                val poster = view?.findViewById<EditText>(R.id.etPoster)?.text
+                val intent = Intent(this@AddFragment.context, PlayActivity::class.java)
+
+                if (!link.isNullOrBlank()) {
+                    intent.data = Uri.parse(link.toString())
+                    intent.putExtra("title", title.toString())
+                    intent.putExtra("poster", poster.toString())
+                    startActivity(intent)
+                }
                 fragManager.popBackStack()
             }
             findViewById<Button>(R.id.btnCancel)?.setOnClickListener {
