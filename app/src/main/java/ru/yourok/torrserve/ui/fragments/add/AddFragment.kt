64bfac9729c515fa0cbc50f83cbc10b36e1683ec
YouROK <8yourok8@mail.ru>
2020-12-14 12:53:22 +0300

    change to api

--- app/src/main/java/ru/yourok/torrserve/ui/fragments/add/AddFragment.kt
+++ app/src/main/java/ru/yourok/torrserve/ui/fragments/add/AddFragment.kt
@@ -1,16 +1,17 @@
 package ru.yourok.torrserve.ui.fragments.add
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import ru.yourok.torrserve.R
 import ru.yourok.torrserve.ext.popBackStackFragment
-import ru.yourok.torrserve.ui.activities.play.PlayActivity
+import ru.yourok.torrserve.server.api.Api
 import ru.yourok.torrserve.ui.fragments.TSFragment
 
 class AddFragment : TSFragment() {
@@ -28,17 +29,14 @@ class AddFragment : TSFragment() {
 
         view?.apply {
             findViewById<Button>(R.id.btnOK)?.setOnClickListener {
-                val link = view?.findViewById<EditText>(R.id.etMagnet)?.text
-                val title = view?.findViewById<EditText>(R.id.etTitle)?.text
-                val poster = view?.findViewById<EditText>(R.id.etPoster)?.text
-                val intent = Intent(this@AddFragment.context, PlayActivity::class.java)
+                val link = view?.findViewById<EditText>(R.id.etMagnet)?.text?.toString() ?: ""
+                val title = view?.findViewById<EditText>(R.id.etTitle)?.text?.toString() ?: ""
+                val poster = view?.findViewById<EditText>(R.id.etPoster)?.text?.toString() ?: ""
 
-                if (!link.isNullOrBlank()) {
-                    intent.data = Uri.parse(link.toString())
-                    intent.putExtra("title", title.toString())
-                    intent.putExtra("poster", poster.toString())
-                    startActivity(intent)
-                }
+                if (!link.isNullOrBlank())
+                    lifecycleScope.launch(Dispatchers.IO) {
+                        Api.addTorrent(link, title, poster, true)
+                    }
                 popBackStackFragment()
             }
             findViewById<Button>(R.id.btnCancel)?.setOnClickListener {
