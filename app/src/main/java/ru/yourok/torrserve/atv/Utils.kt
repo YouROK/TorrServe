194686d16f4bf52bea59b7a40a554870ab82632a
YouROK <8yourok8@mail.ru>
2020-12-25 15:40:57 +0300

    fix open cards from channel

--- app/src/main/java/ru/yourok/torrserve/atv/Utils.kt
+++ app/src/main/java/ru/yourok/torrserve/atv/Utils.kt
@@ -6,6 +6,7 @@ import android.os.Build
 import ru.yourok.torrserve.app.App
 import ru.yourok.torrserve.server.models.torrent.Torrent
 import ru.yourok.torrserve.ui.activities.play.PlayActivity
+import ru.yourok.torrserve.utils.TorrentHelper
 
 object Utils {
 
@@ -13,16 +14,13 @@ object Utils {
         return App.context.packageManager.hasSystemFeature("android.software.leanback") && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
     }
 
-    fun isAmazonDev(): Boolean {
-        return App.context.packageManager.hasSystemFeature("amazon.hardware.fire_tv")
-    }
-
     fun buildPendingIntent(torr: Torrent): Intent {
         val vintent = Intent(App.context, PlayActivity::class.java)
-        vintent.setData(Uri.parse(torr.hash))
         vintent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
         vintent.action = Intent.ACTION_VIEW
+        vintent.setData(Uri.parse(TorrentHelper.getTorrentMagnet(torr)))
         vintent.putExtra("action", "play")
+        vintent.putExtra("hash", torr.hash)
         vintent.putExtra("title", torr.title)
         vintent.putExtra("poster", torr.poster)
         vintent.putExtra("save", false)
