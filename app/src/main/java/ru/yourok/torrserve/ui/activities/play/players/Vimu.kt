beef162d5ab759dad2a0573bbe6425ad17fd87bc
yourok <8yourok8@mail.ru>
2021-01-21 21:21:57 +0300

    fix vimu list

--- app/src/main/java/ru/yourok/torrserve/ui/activities/play/players/Vimu.kt
+++ app/src/main/java/ru/yourok/torrserve/ui/activities/play/players/Vimu.kt
@@ -3,6 +3,7 @@ package ru.yourok.torrserve.ui.activities.play.players
 import android.content.Intent
 import android.net.Uri
 import ru.yourok.torrserve.server.models.torrent.Torrent
+import ru.yourok.torrserve.utils.Mime
 import ru.yourok.torrserve.utils.TorrentHelper
 import java.io.File
 
@@ -10,25 +11,29 @@ object Vimu {
     fun getIntent(torrent: Torrent, index: Int): Intent {
         val link = TorrentHelper.getTorrentPlayLink(torrent, index)
 
+        val file = TorrentHelper.findFile(torrent, index) ?: throw Exception("file in torrent not found")
+        val mime = Mime.getMimeType(file.path)
+
         val intent = Intent(Intent.ACTION_VIEW)
         intent.setPackage("net.gtvbox.videoplayer")
         intent.putExtra("forceresume", true)
         intent.putExtra("forcename", torrent.title)
-        intent.setDataAndType(Uri.parse(link), "application/vnd.gtvbox.filelist")
-
-        val names = ArrayList<String>()
-        val files = ArrayList<String>()
+        intent.setDataAndType(Uri.parse(link), mime)
 
         val torrfiles = TorrentHelper.getPlayableFiles(torrent)
-        torrfiles.forEach {
-            if (it.id >= index) {
-                names.add(File(it.path).name)
-                files.add(TorrentHelper.getFileLink(torrent, it))
+        if (torrfiles.size > 1) {
+            val names = ArrayList<String>()
+            val files = ArrayList<String>()
+            torrfiles.forEach {
+                if (it.id >= index) {
+                    names.add(File(it.path).name)
+                    files.add(TorrentHelper.getFileLink(torrent, it))
+                }
             }
+            intent.setDataAndType(Uri.parse(link), "application/vnd.gtvbox.filelist")
+            intent.putStringArrayListExtra("asusfilelist", files)
+            intent.putStringArrayListExtra("asusnamelist", names)
         }
-
-        intent.putStringArrayListExtra("asusfilelist", files)
-        intent.putStringArrayListExtra("asusnamelist", names)
         return intent
     }
 }
