998e574b1e58f2649b49610fc688135b65f98b7d
nikk gitanes <tsynik@gmail.com>
2021-02-05 18:45:12 +0300

    fix networkonmainthread
    coroutines roxxx =)

--- app/src/main/java/ru/yourok/torrserve/server/local/services/HomeWatch.kt
+++ app/src/main/java/ru/yourok/torrserve/server/local/services/HomeWatch.kt
@@ -7,6 +7,9 @@ import android.content.Intent
 import android.os.Build
 import android.util.Log
 import androidx.tvprovider.media.tv.TvContractCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import ru.yourok.torrserve.BuildConfig
 import ru.yourok.torrserve.R
 import ru.yourok.torrserve.app.App
@@ -20,8 +23,6 @@ class HomeWatch : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         val action = intent.action
         if (action == null || !Utils.isGoogleTV()) return
-//        if (BuildConfig.DEBUG)
-//            Log.d(TAG, "onReceive: $action" + " intent: " + intent.toUri(0))
 
         when (action) {
             TvContractCompat.ACTION_PREVIEW_PROGRAM_BROWSABLE_DISABLED -> {
@@ -31,11 +32,15 @@ class HomeWatch : BroadcastReceiver() {
                     Log.d(TAG, "onReceive: ACTION_WATCH_NEXT_PROGRAM_BROWSABLE_DISABLED, $previewProgramId, $previewInternalId")
                 val hash = ChannelProvider(App.context.getString(R.string.torrents)).findProgramHashById(previewProgramId)
                 if (hash.isNotBlank()) {
-                    if (BuildConfig.DEBUG)
-                        Log.d(TAG, "remove hash: " + hash)
-                    Api.remTorrent(hash)
+                    GlobalScope.launch(Dispatchers.IO) {
+                        try {
+                            Api.remTorrent(hash)
+                            UpdaterCards.updateCards()
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    }
                 }
-                UpdaterCards.updateCards()
             }
             TvContractCompat.ACTION_WATCH_NEXT_PROGRAM_BROWSABLE_DISABLED -> {
                 val watchNextProgramId = intent.getLongExtra(TvContractCompat.EXTRA_PREVIEW_PROGRAM_ID, -1L)
