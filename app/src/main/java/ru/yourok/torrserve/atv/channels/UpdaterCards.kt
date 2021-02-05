d51e388577791b8826ee63a9f953654b3e66ca6f
nikk gitanes <tsynik@gmail.com>
2021-02-05 16:38:40 +0300

    update HomeWatch

--- app/src/main/java/ru/yourok/torrserve/atv/channels/UpdaterCards.kt
+++ app/src/main/java/ru/yourok/torrserve/atv/channels/UpdaterCards.kt
@@ -19,7 +19,6 @@ object UpdaterCards {
                 isUpdate = true
             }
 
-            
             ChannelService.updateChannels()
             isUpdate = false
         }
--- app/src/main/java/ru/yourok/torrserve/server/local/services/HomeWatch.kt
+++ app/src/main/java/ru/yourok/torrserve/server/local/services/HomeWatch.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.media.tv.TvContract
 import android.os.Build
 import android.util.Log
+import ru.yourok.torrserve.BuildConfig
 import ru.yourok.torrserve.atv.Utils
 import ru.yourok.torrserve.atv.channels.UpdaterCards
 
@@ -15,19 +16,27 @@ class HomeWatch : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         val action = intent.action
         if (action == null || !Utils.isGoogleTV()) return
-//        Log.d("*****", "onReceive: $action")
+//        if (BuildConfig.DEBUG)
+//            Log.d(TAG, "onReceive: $action" + " intent: " + intent.toUri(0))
+
         when (action) {
-//            TvContract.ACTION_WATCH_NEXT_PROGRAM_BROWSABLE_DISABLED -> {
-//                val previewProgramId = intent.getLongExtra(TvContract.EXTRA_PREVIEW_PROGRAM_ID, -1L)
-//                val previewInternalId = intent.getLongExtra(TvContract.EXTRA_WATCH_NEXT_PROGRAM_ID, -1L)
-//                Log.d("*****", "onReceive: ACTION_WATCH_NEXT_PROGRAM_BROWSABLE_DISABLED, $previewProgramId, $previewInternalId")
-//            }
             TvContract.ACTION_PREVIEW_PROGRAM_BROWSABLE_DISABLED -> {
+                val previewProgramId = intent.getLongExtra(TvContract.EXTRA_PREVIEW_PROGRAM_ID, -1L)
+                val previewInternalId = intent.getLongExtra(TvContract.EXTRA_WATCH_NEXT_PROGRAM_ID, -1L)
+                if (BuildConfig.DEBUG)
+                    Log.d(TAG, "onReceive: ACTION_WATCH_NEXT_PROGRAM_BROWSABLE_DISABLED, $previewProgramId, $previewInternalId")
+                UpdaterCards.updateCards()
+            }
+            TvContract.ACTION_WATCH_NEXT_PROGRAM_BROWSABLE_DISABLED -> {
                 val watchNextProgramId = intent.getLongExtra(TvContract.EXTRA_PREVIEW_PROGRAM_ID, -1L)
                 val watchNextInternalId = intent.getLongExtra(TvContract.EXTRA_WATCH_NEXT_PROGRAM_ID, -1L)
-                Log.d("*****", "onReceive: ACTION_PREVIEW_PROGRAM_BROWSABLE_DISABLED, $watchNextProgramId, $watchNextInternalId")
-//                UpdaterCards.updateCards()
+                if (BuildConfig.DEBUG)
+                    Log.d(TAG, "onReceive: ACTION_PREVIEW_PROGRAM_BROWSABLE_DISABLED, $watchNextProgramId, $watchNextInternalId")
             }
         }
+    }
+
+    companion object {
+        private const val TAG = "HomeWatch"
     }
 }
