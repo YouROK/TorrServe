71c9e233490f352d6b11ee082e2f542001c99497
nikk gitanes <tsynik@gmail.com>
2023-04-05 19:55:22 +0300

    update errors

--- app/src/main/java/ru/yourok/torrserve/ui/activities/play/ReturnErrors.kt
+++ app/src/main/java/ru/yourok/torrserve/ui/activities/play/ReturnErrors.kt
@@ -5,12 +5,12 @@ import ru.yourok.torrserve.app.App
 
 data class ReturnError(val errCode: Int, val errMessage: String)
 
-val ErrIntentNull = ReturnError(-1, "intent is null")
-val ErrUnknownCmd = ReturnError(-2, "unknown command")
-val ErrProcessCmd = ReturnError(-3, "error when processing cmd, see logcat")
-val ErrEmptyTorrent = ReturnError(-4, "torrent link empty")
+val ErrIntentNull = ReturnError(-1, App.context.getString(R.string.error_intent_null))
+val ErrUnknownCmd = ReturnError(-2, App.context.getString(R.string.error_unknown_command))
+val ErrProcessCmd = ReturnError(-3, App.context.getString(R.string.error_process_cmd))
+val ErrEmptyTorrent = ReturnError(-4, App.context.getString(R.string.error_empty_link))
 val ErrLoadTorrent = ReturnError(-5, App.context.getString(R.string.error_add_torrent))
 val ErrLoadTorrentInfo = ReturnError(-6, App.context.getString(R.string.error_retrieve_torrent_info))
-val ErrUserStop = ReturnError(-7, "user stop")
+val ErrUserStop = ReturnError(-7, App.context.getString(R.string.error_user_cancel))
 val ErrTorrServerNotResponding = ReturnError(-8, App.context.getString(R.string.server_not_responding))
 val ErrLoadTorrentFiles = ReturnError(-9, App.context.getString(R.string.error_retrieve_torrent_file))
