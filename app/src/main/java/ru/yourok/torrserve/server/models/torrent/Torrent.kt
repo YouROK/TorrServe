1fefd89ea4b06ea35ae74ce38fe272fe889e5205
nikk gitanes <tsynik@gmail.com>
2024-04-15 20:35:31 +0300

    make category nullable type
    fix FC

--- app/src/main/java/ru/yourok/torrserve/server/models/torrent/Torrent.kt
+++ app/src/main/java/ru/yourok/torrserve/server/models/torrent/Torrent.kt
@@ -3,7 +3,7 @@ package ru.yourok.torrserve.server.models.torrent
 data class Torrent(
     var title: String,
     var poster: String,
-    var category: String,
+    var category: String?,
     var data: String,
     var timestamp: Long,
     var name: String,
